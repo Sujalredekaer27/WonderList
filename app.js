bd24932d32811bcb05e2c18a63d7bd436747474d
Sujal Sanjay Redekar <sujalredekar27@gmail.com>
2026-02-23 21:57:04 +0530

    Add some changes on the navbar design to make it more beautiful

--- app.js
+++ app.js
@@ -51,9 +51,9 @@ const sessionOptions = {
     }
 }
 
-app.get("/",(req,res) => {
-    res.send("hihe");
-});
+// app.get("/",(req,res) => {
+//     res.send("hihe");
+// });
 
 app.use(session(sessionOptions));
 app.use(flash());
